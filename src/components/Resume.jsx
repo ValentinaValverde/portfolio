d7775879e8bccba1d194799cf10cfcf1a86b8dad
Valentina Valverde <vvgreenville@gmail.com>
2023-12-06 09:49:15 -0500

    Attempted to add Resume pdf.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,25 @@
-import ResumePDF from "../assets/ResumePDF.pdf";
 import React from "react";
+import ResumePDF from "../assets/ResumePDF.pdf";
 
 export function Resume() {
   return (
     <>
-      <p>Resume</p>
-      {/* <iframe src="ResumePDF" /> */}
+      <div className="resume_container">
+        <p className="title">
+          <i>resume </i>
+        </p>
+        <iframe src="ResumePDF" className="resume_pdf" />
+        {/* <PDFViewer /> */}
+      </div>
     </>
   );
 }
+
+export const PDFViewer = () => {
+  return (
+    <div>
+      <iframe src="../assets/ResumePDF.pdf" />
+    </div>
+  );
+};
+export default PDFViewer;
